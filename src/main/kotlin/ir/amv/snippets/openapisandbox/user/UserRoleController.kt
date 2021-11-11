58fd6dee8a9c66182f11b63f5cfedc08a1c4c0f7
Amir Vosough <amir@locorepo.com>
2021-11-11 16:39:12 +0100

    :sparkles: working generated spec!

--- src/main/kotlin/ir/amv/snippets/openapisandbox/user/UserRoleController.kt
+++ src/main/kotlin/ir/amv/snippets/openapisandbox/user/UserRoleController.kt
@@ -1,28 +1,51 @@
 package ir.amv.snippets.openapisandbox.user
 
 import ir.amv.os.snippets.generated.api.UserRoleApi
-import ir.amv.os.snippets.generated.model.UserDto
 import ir.amv.os.snippets.generated.model.UserRoleDto
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
 
 @RestController
 class UserRoleController(
+    private val userRepo: UserRepo,
     private val repo: UserRoleRepository
-): UserRoleApi {
+) : UserRoleApi {
     override fun createUserRole(userId: UUID, userRoleDto: UserRoleDto): ResponseEntity<UUID> {
         return ResponseEntity.ok(
-            repo.save()
+            repo.save(
+                userRoleDto.toEntity(
+                    userRepo.findByIdOrNull(userId)!!
+                )
+            ).id
         )
     }
 
-    override fun deleteUserRole(userId: UUID?, id: UUID?): ResponseEntity<UserDto> {
-        return super.deleteUserRole(userId, id)
+    override fun deleteUserRole(userId: UUID?, id: UUID?): ResponseEntity<UserRoleDto> {
+        return ResponseEntity.ok(
+            repo.findByIdOrNull(id)!!
+                .also { repo.delete(it) }
+                .toDto()
+        )
     }
 
-    override fun getUserRoles(userId: UUID?): ResponseEntity<MutableList<UserRoleDto>> {
-        return super.getUserRoles(userId)
+    override fun getUserRoles(userId: UUID): ResponseEntity<List<UserRoleDto>> {
+        return ResponseEntity.ok(
+            repo.findByUserId(userId)
+                .map { it.toDto() }
+        )
+    }
+
+    override fun updateUserRole(userId: UUID, id: UUID, userRoleDto: UserRoleDto): ResponseEntity<UserRoleDto> {
+        return ResponseEntity.ok(
+            repo.findByIdOrNull(id)!!
+                .also {
+                    it.name = userRoleDto.name
+                    repo.save(it)
+                }
+                .toDto()
+        )
     }
 
     private fun UserRoleEntity.toDto() =
@@ -30,9 +53,10 @@ class UserRoleController(
             .id(id)
             .name(name)
 
-    private fun UserRoleDto.toEntity() =
+    private fun UserRoleDto.toEntity(user: UserEntity) =
         UserRoleEntity(
             id = id,
-            name = name
+            name = name,
+            user = user
         )
 }
--- src/main/kotlin/ir/amv/snippets/openapisandbox/user/UserRoleEntity.kt
+++ src/main/kotlin/ir/amv/snippets/openapisandbox/user/UserRoleEntity.kt
@@ -4,12 +4,17 @@ import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 import javax.persistence.Entity
 import javax.persistence.Id
+import javax.persistence.ManyToOne
 
 @Entity
 class UserRoleEntity(
     @Id
     var id: UUID,
-    var name: String
+    var name: String,
+    @ManyToOne
+    var user: UserEntity
 )
 
-interface UserRoleRepository: JpaRepository<UserRoleEntity, UUID>
+interface UserRoleRepository : JpaRepository<UserRoleEntity, UUID> {
+    fun findByUserId(userId: UUID): List<UserRoleEntity>
+}
